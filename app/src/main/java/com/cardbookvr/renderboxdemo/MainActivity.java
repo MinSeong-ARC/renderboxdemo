e9b5d9933863adb60401a2629b0cba63923575fe
Jonathan Linowes <jonathan@parkerhill.com>
2016-04-16 01:55:53 -0400

    initial commit

---
+++ app/src/main/java/com/cardbookvr/renderboxdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cardbookvr.renderboxdemo;
+
+import android.os.Bundle;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+
+public class MainActivity extends CardboardActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
