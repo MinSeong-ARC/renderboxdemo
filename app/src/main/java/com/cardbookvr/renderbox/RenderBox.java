f0b796358cebcb2eca3e68fad7bad5cbf8f2fc87
Jonathan Linowes <jonathan@parkerhill.com>
2016-04-16 02:35:47 -0400

    new project with renderbox and IRenderBox interface

---
+++ app/src/main/java/com/cardbookvr/renderbox/RenderBox.java
@@ -0,0 +1,76 @@
+package com.cardbookvr.renderbox;
+
+import android.app.Activity;
+import android.opengl.GLES20;
+import android.opengl.GLU;
+import android.util.Log;
+
+import com.google.vrtoolkit.cardboard.CardboardView;
+import com.google.vrtoolkit.cardboard.Eye;
+import com.google.vrtoolkit.cardboard.HeadTransform;
+import com.google.vrtoolkit.cardboard.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+/**
+ * Created by Schoen and Jonathan on 4/16/2016.
+ */
+public class RenderBox implements CardboardView.StereoRenderer {
+    private static final String TAG = "RenderBox";
+
+    public static RenderBox instance;
+    public Activity mainActivity;
+    IRenderBox callbacks;
+
+    public RenderBox(Activity mainActivity, IRenderBox callbacks){
+        instance = this;
+        this.mainActivity = mainActivity;
+        this.callbacks = callbacks;
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int i, int i1) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+
+    /**
+     * Checks if we've had an error inside of OpenGL ES, and if so
+     what that error is.
+     * @param label Label to report in case of error.
+     */
+    public static void checkGLError(String label) {
+        int error;
+        while ((error = GLES20.glGetError()) !=
+                GLES20.GL_NO_ERROR) {
+            String errorText = String.format("%s: glError %d, %s",
+                    label, error, GLU.gluErrorString(error));
+            Log.e(TAG, errorText);
+            throw new RuntimeException(errorText);
+        }
+    }
+}
--- app/src/main/java/com/cardbookvr/renderboxdemo/MainActivity.java
+++ app/src/main/java/com/cardbookvr/renderboxdemo/MainActivity.java
@@ -2,13 +2,36 @@ package com.cardbookvr.renderboxdemo;
 
 import android.os.Bundle;
 
+import com.cardbookvr.renderbox.IRenderBox;
+import com.cardbookvr.renderbox.RenderBox;
 import com.google.vrtoolkit.cardboard.CardboardActivity;
+import com.google.vrtoolkit.cardboard.CardboardView;
 
-public class MainActivity extends CardboardActivity {
+public class MainActivity extends CardboardActivity implements IRenderBox {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardboardView cardboardView = (CardboardView) findViewById(R.id.cardboard_view);
+        cardboardView.setRenderer(new RenderBox(this,this));
+        setCardboardView(cardboardView);
+
+    }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void preDraw() {
+
+    }
+
+    @Override
+    public void postDraw() {
+
     }
 }
